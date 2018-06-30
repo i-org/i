ea9e0e990efa1adb9423043d7f8fd964b9672a7a
黑传说(Atlas Jobinson) <jobinson99@gmail.com>
2018-06-30 15:40:31 +0800

    改进设计

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 module.exports = require('./lib/i-core'); 
+
+
+/* 包信息 */
+
+
+
+
+/* 分析器 */
+
+var parser = { };
+
+parser.tokens = {
+
+"regex": "";
+"comment": "";
+"ignored": ""
+
+};
+
+/* 优先度 */
+
+
+
